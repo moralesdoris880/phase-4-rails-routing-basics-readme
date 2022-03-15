8797fb4bccebcf93b285cb91f815c83da80a410c
Doris Morales <moralesdoris880@gmail.com>
2022-03-15 13:17:39 -0400

    Completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-puts '🧀 Seeding cheeses...'
+#puts '🧀 Seeding cheeses...'
 
-Cheese.create!(name: 'Cheddar', price: 3, is_best_seller: true)
-Cheese.create!(name: 'Pepper Jack', price: 4, is_best_seller: true)
-Cheese.create!(name: 'Limburger', price: 8, is_best_seller: false)
+#Cheese.create!(name: 'Cheddar', price: 3, is_best_seller: true)
+#Cheese.create!(name: 'Pepper Jack', price: 4, is_best_seller: true)
+#Cheese.create!(name: 'Limburger', price: 8, is_best_seller: false)
 
-puts '✅ Done seeding'
+#puts '✅ Done seeding'
